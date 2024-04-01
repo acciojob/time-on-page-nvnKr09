fcebdd4bbe7199caad1a5f85a7b2540523282a92
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2024-04-01 23:15:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-
-import React from "react";
+import {React, useState, useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  const [seconds, setSeconds] = useState(0);
+  
+  useEffect(()=>{
+    setTimeout(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+  })
+  
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>You've been on this page for {seconds} seconds</p>
     </div>
   )
 }
