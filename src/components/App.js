72f70574d809f5b3156bae459102c20a14722741
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2024-04-01 23:34:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
-import {React, useState, useEffect} from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
-
 const App = () => {
-  
   const [seconds, setSeconds] = useState(0);
-  
-  useEffect(()=>{
-    setTimeout(() => {
-      setSeconds((seconds) => seconds + 1);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
     }, 1000);
-  })
-  
-  
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div>
-    {`You've been on this page for ${seconds} seconds`}
+      <p>You've been on this page for {seconds} seconds.</p>
     </div>
-  )
+  );
 }
-
 export default App
